555aee5b3f7382e545800f7327856ea1f64840a1
Kjbjbjb <kgood10369@gmail.com>
2021-02-08 21:34:29 +0900

    20210208

--- baekjoon/basic/exam.java
+++ baekjoon/basic/exam.java
@@ -1,14 +1,48 @@
-import java.util.Scanner;
-import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.util.HashSet;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+class Student implements Comparable<Student>{
+	private String name;
+	private int num;
+	
+	public Student(String name,int num){
+		this.name=name;
+		this.num=num;
+	}
+	public int getnum() {
+		return this.num;
+	}
+	@Override
+	public int compareTo(Student student) {
+		if (this.num>student.num) {
+			return 1;
+		}
+		else if (this.num<student.num) {
+			return -1;
+		}
+		else {
+			return 0;
+		}
+	}
+	public String print() {
+		return this.name+"이는 "+this.num;
+	}
+}
+
 public class exam {
-
-	public static void main(String[] args) throws IOException {
-		boolean [] a=new boolean[1];
+	public static void main(String[] args) {
+		Student [] jb= {new Student("준범",15),new Student("정엽",10),new Student("지환",12),new Student("창훈",17)};
+		Arrays.sort(jb);
+		for (int i=0;i<4;i++) {
+			System.out.println(jb[i].print());
+		}
 		
+}
+}
 
-		System.out.println(a[0]);
-}
-}
+
+
--- baekjoon/basic/보물_1026.java
+++ baekjoon/basic/보물_1026.java
@@ -3,48 +3,27 @@ import java.util.Arrays;
 import java.io.*;
 public class º¸¹°_1026 {
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		int n=Integer.parseInt(br.readLine());
-		int [] A=new int [n];
-		int [] B=new int [n];
-		int [] a=new int [n];
-		int [] b=new int [n];
-		int [] num=new int [n];
-		boolean [] check=new boolean[n];
+		st=new StringTokenizer(br.readLine());
+		int []a=new int[n];
+		for (int i=0;i<n;i++) {
+			a[i]=Integer.parseInt(st.nextToken());
+		}
+		int []b=new int[n];
 		st=new StringTokenizer(br.readLine());
 		for (int i=0;i<n;i++) {
-			A[i]=Integer.parseInt(st.nextToken());
+			b[i]=Integer.parseInt(st.nextToken());
 		}
-		Arrays.sort(A);
-		st=new StringTokenizer(br.readLine());
-		for (int i=0;i<n;i++) {
-			B[i]=Integer.parseInt(st.nextToken());
-		}
-		b=B.clone();
-		Arrays.sort(B);
-		for (int i=0;i<n;i++) {
-			int temp=B[i];
-			for (int j=0;j<n;j++) {
-				if (check[j]) continue;
-				if (temp==b[j]) {
-					check[j]=true;
-					num[j]=i;
-					break;
-				}
-			}
-		}
-		for (int i=0;i<n;i++) {
-			a[i]=A[n-num[i]-1];
-		}
+		Arrays.sort(b);
+		Arrays.sort(a);
 		int ans=0;
 		for (int i=0;i<n;i++) {
-			ans+=(a[i]*b[i]);
+			ans+=a[n-i-1]*b[i];
 		}
 		System.out.println(ans);
-		
-
-	}
-
 }
+}
+
