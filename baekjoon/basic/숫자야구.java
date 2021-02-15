928d2861edf3bf73a79eb27d6c11291297dfb303
Kjbjbjb <kgood10369@gmail.com>
2021-02-16 00:26:42 +0900

    20210216

--- baekjoon/basic/숫자야구.java
+++ baekjoon/basic/숫자야구.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
 import java.util.HashSet;
 import java.util.Random;
-import java.util.Arrays;
 public class 숫자야구 {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		num a=new num();
-		a.printnum();
-		System.out.println(Arrays.toString(makeNum()));
-		
-
+		num a=new num(makeNum());
+		while (true) {
+			System.out.println("숫자를 입력하세요");
+			int [] temp=a.checknum(sc.next());
+			if (temp[0]==4) {
+				System.out.println("정답을 맞추셨습니다"); 
+				break;
+			}
+			else System.out.println(String.format("%dS %dB",temp[0],temp[1]));
+		}
 	}
 	public static int[] makeNum() {
 		int [] number=new int[4];
@@ -28,12 +32,24 @@ public class 숫자야구 {
 
 }
 class num{
-	private String nums;
+	private int [] nums;
 	
-	public num() {
-		this.nums="TLQKf";
+	public num(int [] nums) {
+		this.nums=nums;
 	}
-	public void printnum(){
-		System.out.println(nums);
+	public int [] checknum(String n){
+		int s=0;
+		int b=0;
+		for (int i=0;i<4;i++) {
+			if ((n.charAt(i)-48)==nums[i]) s++;
+		}
+		for (int i=0;i<4;i++) {
+			for(int j=0;j<4;j++) {
+				if (i==j) continue;
+				if ((n.charAt(i)-48)==nums[j]) b++;
+			}
+		}
+		return new int[] {s,b};
+
 	}
 }
