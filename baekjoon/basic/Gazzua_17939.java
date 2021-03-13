ea36e0326ba57185f84f21dc516bce3374dc863e
junbeom-kang <kgood10369@gmail.com>
2021-03-14 04:35:46 +0900

    no message

--- baekjoon/basic/Gazzua_17939.java
+++ baekjoon/basic/Gazzua_17939.java
@@ -1,7 +1,29 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Gazzua17939 {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    
+public class Gazzua_17939 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int ans=0;
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        st=new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int high = arr[n - 1];
+        for (int i = n - 2; i > -1; i--) {
+            if (arr[i] > high) {
+                high = arr[i];
+            } else {
+                ans += high - arr[i];
+            }
+        }
+        System.out.println(ans);
+
+    }
+
 }
--- baekjoon/basic/흩날리는시험지_17951.java
+++ baekjoon/basic/흩날리는시험지_17951.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class 흩날리는시험지_17951 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 흩날리는시험지_17951 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int ans=0;
+        String[] s = br.readLine().split(" ");
+        int n = Integer.parseInt(s[0]);
+        int m = Integer.parseInt(s[1]);
+        st = new StringTokenizer(br.readLine());
+        int [] arr=new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int left=0;
+        int right=2000000;
+        while (left<=right){
+            int min=(left+right)/2;
+            int cnt=0;
+            int t=0;
+            for (int i = 0; i < n; i++) {
+                t+= arr[i];
+                if (t >= min) {
+                    cnt+=1;
+                    t=0;
+                }
+                }
+            if (cnt >= m) {
+                ans = min;
+                left = min + 1;
+            } else {
+                right=min-1;
+            }
+            }
+
+        System.out.println(ans);
+    }
 }
