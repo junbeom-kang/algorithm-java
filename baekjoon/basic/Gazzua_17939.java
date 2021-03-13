05374a600e235028c966ea3a777e972389b7c3c6
junbeom-kang <kgood10369@gmail.com>
2021-03-14 04:35:43 +0900

    no message

---
+++ baekjoon/basic/Gazzua_17939.java
@@ -0,0 +1,7 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Gazzua17939 {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+}
---
+++ baekjoon/basic/흩날리는시험지_17951.java
@@ -0,0 +1,2 @@
+package PACKAGE_NAME;public class 흩날리는시험지_17951 {
+}
