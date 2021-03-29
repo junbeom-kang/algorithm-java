c09d9fa5a460fcf0c8cefee8a1e71cd35acf31fd
junbeom-kang <kgood10369@gmail.com>
2021-03-30 02:44:06 +0900

    no message

--- baekjoon/basic/카드정렬하기_1715.java
+++ baekjoon/basic/카드정렬하기_1715.java
@@ -4,21 +4,24 @@ import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 
 public class 카드정렬하기_1715 {
+    static int ans=0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         PriorityQueue<Integer> priorityQueue=new PriorityQueue<>();
         int n=Integer.parseInt(br.readLine());
-        int ans=0;
         for (int i = 0; i < n; i++) {
             priorityQueue.add(Integer.parseInt(br.readLine()));
         }
-        while (priorityQueue.size()!=1){
-            int temp=priorityQueue.poll()+priorityQueue.poll();
-            priorityQueue.add(temp);
-            ans+=temp;
+        if (n == 1) {
+            System.out.println(0);
+        } else {
+            while (priorityQueue.size() > 1) {
+                int temp=priorityQueue.poll()+priorityQueue.poll();
+                priorityQueue.add(temp);
+                ans+=temp;
+            }
+            System.out.println(ans);
         }
-        System.out.println(ans);
-        
 
     }
 }
