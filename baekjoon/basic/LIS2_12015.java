f5e6ff190fbffeeb9cd97702de5c2076fae5b4a9
junbeom-kang <kgood10369@gmail.com>
2021-04-21 03:22:26 +0900

    no message

--- baekjoon/basic/LIS2_12015.java
+++ baekjoon/basic/LIS2_12015.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class LIS2_12015 {
     public static void main(String[] args) throws IOException {
@@ -9,9 +10,32 @@ public class LIS2_12015 {
         int n = Integer.parseInt(br.readLine());
         String[] s = br.readLine().split(" ");
         ArrayList<temp> dp=new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-
+        dp.add(new temp(Integer.parseInt(s[0]),1));
+        int size=1;
+        for (int i = 1; i < n; i++) {
+            int t=Integer.parseInt(s[i]);
+            if (t > dp.get(size - 1).num) {
+                dp.add(new temp(t, dp.get(size - 1).count + 1));
+                size += 1;
+            } else {
+                bs(0,size-1,t,dp);
+            }
         }
+        System.out.println(dp.get(size-1).count);
+
+    }
+    static void bs(int left,int right,int num,ArrayList<temp> dp) {
+        int ans=0;
+        while (left <= right) {
+            int mid=(left+right)/2;
+            if (num <= dp.get(mid).num) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left=mid+1;
+            }
+        }
+        dp.get(ans).num=num;
     }
 
     static class temp {
--- baekjoon/exercise/exx/te.java
+++ baekjoon/exercise/exx/te.java
@@ -1,10 +1,1 @@
-package exx;
 
-public class te {
-    public static void main(String[] args) {
-        Penguin penguin = new Penguin();
-        Penguin pingu = new Penguin();
-        penguin.name = "»Ç·Î·Î";
-        pingu.h
-    }
-}
