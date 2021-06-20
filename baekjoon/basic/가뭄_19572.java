1b7e6b426278d4044f1b6d0e23afd2c6434fb115
junbeom-kang <kgood10369@gmail.com>
2021-06-20 21:03:06 +0900

    no message

---
+++ baekjoon/basic/가뭄_19572.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class °¡¹³_19572 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s=br.readLine().split(" ");
+        float a = Integer.parseInt(s[0]);
+        float b = Integer.parseInt(s[1]);
+        float c = Integer.parseInt(s[2]);
+        float d,e,f;
+        d = (a + c - b) / 2;
+        f=c-d;
+        e=b-f;
+        if (d > 0 && e > 0 && f > 0) {
+            System.out.println(1);
+            System.out.println(String.format("%.1f %.1f %.1f", d, e, f));
+        } else {
+            System.out.println(-1);
+        }
+
+    }
+}
