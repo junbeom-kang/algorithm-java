8663fde283542516719491bf06b6223bf33489cd
junbeom-kang <kgood10369@gmail.com>
2021-06-30 18:54:43 +0900

    나중에 다시풀어보기

--- baekjoon/basic/마스크가_필요해_19580.java
+++ baekjoon/basic/마스크가_필요해_19580.java
@@ -1,31 +1,49 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.TreeSet;
+import java.util.*;
 
 public class 마스크가_필요해_19580 {
     static String[] s;
-    static int n, m;
-    static Pair p;
+    static int n, m,ans;
+    static PriorityQueue<Pair> pq = new PriorityQueue<>();
+    static ArrayList<market> arr=new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         s = br.readLine().split(" ");
         n = Integer.parseInt(s[0]);
         m = Integer.parseInt(s[1]);
-        Pair[] people = new Pair[n];
+
 
         for (int i = 0; i < n; i++) {
             s = br.readLine().split(" ");
-            people[i]=new Pair(Long.parseLong(s[0]),Long.parseLong(s[1]));
+            pq.add(new Pair(Long.parseLong(s[0]), Long.parseLong(s[1])));
         }
-        Arrays.sort(people);
-        System.out.println(people.toString());
-        for (Pair person : people) {
-            System.out.println(person.right);
+        for (int i = 0; i < m; i++) {
+            s=br.readLine().split(" ");
+            arr.add(new market(Long.parseLong(s[0]), Integer.parseInt(s[1])));
         }
+        Collections.sort(arr);
+        for (market market : arr) {
+            System.out.println(String.format("%d !!!!!",market.price));
+            while (!pq.isEmpty()) {
+                System.out.println(market.stock);
+                if (market.stock == 0) break;
+                if (market.price >= pq.peek().left && market.price <= pq.peek().right) {
+                    ans += 1;
+                    market.stock -= 1;
+                    pq.poll();
+                } else if(pq.peek().right<market.price){
+                    pq.poll();
+                } else if (pq.peek().right> market.price) {
+                    break;
+                }
+            }
+            System.out.println(market.stock);
+            System.out.println("next market");
+        }
+        System.out.println(ans);
 
 
     }
@@ -46,7 +64,28 @@ public class 마스크가_필요해_19580 {
             } else if (this.right == p.right) {
                 return 0;
             } else {
+                return -1;
+            }
+        }
+    }
+
+    static class market implements Comparable<market>{
+        long price;
+        int stock;
+
+        public market(long price, int stock) {
+            this.price = price;
+            this.stock = stock;
+        }
+
+        @Override
+        public int compareTo(market o) {
+            if (this.price > o.price) {
+                return 1;
+            } else if (this.price == o.price) {
                 return 0;
+            } else {
+                return -1;
             }
         }
     }
