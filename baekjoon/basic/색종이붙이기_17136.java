5afb5b6ffd87a3e4f00f7847f389b0f248084963
junbeom-kang <kgood10369@gmail.com>
2021-07-12 23:38:25 +0900

    no message

--- baekjoon/basic/색종이붙이기_17136.java
+++ baekjoon/basic/색종이붙이기_17136.java
@@ -5,27 +5,69 @@ import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class 색종이붙이기_17136 {
+    static boolean[][] visited = new boolean[10][10];
+    static String[][] arr = new String[20][20];
+    static HashMap<Integer, Integer> hs = new HashMap<>();
     public static void main(String[] args) throws IOException {
+        int ans=0;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        HashMap<Integer, Integer> hs = new HashMap<>();
+
         for (int i = 1; i < 6; i++) {
             hs.put(i, 5);
         }
-        String[][] arr = new String[20][20];
         StringTokenizer st;
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 10; i++) {
             st=new StringTokenizer(br.readLine());
-            for (int j = 0; j < 20; j++) {
+            for (int j = 0; j < 10; j++) {
                 arr[i][j]=st.nextToken();
             }
         }
-        for (int i = 0; i < 20; i++) {
-            for (int j = 0; j < 20; j++) {
-                start(i,j);
+        end:for (int k = 5; k >0; k--) {
+            gg:for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    if (!visited[i][j]) {
+                        start(i,j,k);
+                    }
+                    if (hs.get(k) == 0 & k!=1) {
+                        break gg;
+                    }
+                    if (hs.get(k) == -1) {
+                        ans=-1;
+                        break end;
+                    }
+                }
             }
         }
 
+        for (Integer integer : hs.keySet()) {
+            if (hs.get(integer) < 0) {
+                ans = -1;
+                break;
+            } else {
+                ans += (5 - hs.get(integer));
+            }
+        }
+        System.out.println(ans);
+
 
     }
+    static boolean start(int i, int j,int k) {
+        for (int l = 0; l < k; l++) {
+            for (int m = 0; m < k; m++) {
+                if (i + l >= 10 || j + m >= 10 || !arr[i+l][j+m].equals("1")||visited[i+l][j+m]) {
+                    return false;
+                }
+            }
+        }
+        for (int l = 0; l < k; l++) {
+            for (int m = 0; m < k; m++) {
+                visited[i + l][j + m] = true;
+            }
+        }
+        hs.put(k, hs.get(k) - 1);
+        return true;
+
+    }
+
 
 }
